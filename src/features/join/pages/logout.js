b307619c5d89b36f42e82e887a69b5ce9184418a
Dmitro Zherebko <kraken@live.ru>
2018-07-15 16:44:36 +0300

    Update logout.js

--- src/features/join/pages/logout.js
+++ src/features/join/pages/logout.js
@@ -1,61 +1,70 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { compose, withHandlers, withState } from 'recompose'
+import React from "react";
+import { connect } from "react-redux";
 
-import { Col, Row } from 'styled-components-layout'
-import { Card, Button } from 'ui/atoms'
-import { PrimitiveFooter } from 'ui/organisms'
-import { Container, CenterContentTemplate } from 'ui/templates'
+import { Col, Row } from "styled-components-layout";
+import { PrimitiveFooter } from "ui/organisms";
+import { accountReset } from "features/account";
+import { Card, Button } from "ui/atoms";
+import { Container, CenterContentTemplate } from "ui/templates";
 
-import { accountReset } from 'features/account'
+const RESET_TIMEOUT = 500;
 
+export const LogoutView = class extends React.Component {
+  state = {
+    isLogouting: true
+  };
+  setLogouting = (val = false) => this.setState({ isLogouting: val });
+  logout = () => () => {
+    const { history, onReset } = this.props;
+    this.setLogouting(true);
+    setTimeout(() => {
+      onReset();
+      setTimeout(() => history.push("/"), 150);
+    }, RESET_TIMEOUT);
+  };
+  back = () => {
+    this.props.history.goBack();
+  };
 
-const RESET_TIMEOUT = 500
+  render() {
+    const { isLogouting } = this.state;
+    const { logout, back } = this;
 
-const mapDispatchToProps = (dispatch) => ({
-  onReset: () => dispatch(accountReset),
-})
+    return this.renderTemplate({ isLogouting, logout, back });
+  }
+  renderLoginButton({ isLogouting, logout, back }) {
+    return (
+      <Row gap="1rem">
+        <Button.Primary grow disabled={isLogouting} onClick={logout}>
+          {isLogouting ? "Logging out…" : "Logout"}
+        </Button.Primary>
+        <Button disabled={isLogouting} onClick={back}>
+          Back
+        </Button>
+      </Row>
+    );
+  }
+  renderTemplate(isLogouting, logout, back) {
+    return (
+      <CenterContentTemplate footer={<PrimitiveFooter />}>
+        <Container justify="center" align="center">
+          <Col align="center" width="40rem">
+            <Card>
+              <Row padding="0 0 2rem 0">Stop session?</Row>
+              {this.renderLoginButton({ isLogouting, logout, back })}
+            </Card>
+          </Col>
+        </Container>
+      </CenterContentTemplate>
+    );
+  }
+};
 
-const enhance = compose(
-  connect(null, mapDispatchToProps),
-  withState('isLogouting', 'setLogouting', false),
-  withHandlers({
-    logout: ({ history, setLogouting, onReset }) => () => {
-      setLogouting(true)
+const mapDispatchToProps = dispatch => ({
+  onReset: () => dispatch(accountReset)
+});
 
-      setTimeout(() => {
-        onReset()
-        setTimeout(() => history.push('/'), 150)
-      }, RESET_TIMEOUT)
-    },
-    back: ({ history }) => () => {
-      history.goBack()
-    },
-  }),
-)
-
-export const LogoutView = ({ logout, back, isLogouting }) => (
-  <CenterContentTemplate footer={<PrimitiveFooter />}>
-    <Container justify="center" align="center">
-      <Col align="center" width="40rem">
-        <Card>
-          <Row padding="0 0 2rem 0">
-            Stop session?
-          </Row>
-          <Row gap="1rem">
-            <Button.Primary
-              grow
-              disabled={isLogouting}
-              onClick={logout}
-            >
-              {isLogouting ? 'Logging out…' : 'Logout'}
-            </Button.Primary>
-            <Button disabled={isLogouting} onClick={back}>Back</Button>
-          </Row>
-        </Card>
-      </Col>
-    </Container>
-  </CenterContentTemplate>
-)
-
-export const LogoutPage = enhance(LogoutView)
+export const LogoutPage = connect(
+  null,
+  mapDispatchToProps
+)(LogoutView);
